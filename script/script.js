d73a9238ee35fe06c67a9df655e6e4bee51e7cf0
Ajoke752 <ajokeabdulrasaq2@gmail.com>
2025-05-04 12:42:28 -0700

    My Fifth commit
    I made some validations on my form

--- script/script.js
+++ script/script.js
@@ -4,33 +4,16 @@ let hiddenSection = document.querySelector('#hidden-section')
 let viewDetails = document.querySelector('#view-details')
 let closeButton = document.querySelector('#close-button')
 let form = document.querySelector('form')
+let resultView = document.querySelector('#result-view')
+let successMessage = document.querySelector('#success-tag')
 
 const showHiddenSection = (e) => {
   e.preventDefault()
-  hiddenSection.style.display = 'flex'
-  hiddenSection.style.flexDirection = 'column'
-  hiddenSection.style.alignItems = 'center'
-  hiddenSection.style.justifyContent = 'center'
-  viewDetails.addEventListener('click', handleFormDetails)
-  closeButton.addEventListener('click', closeHiddenSection)
-}
 
-const closeHiddenSection = (e) => {
-  e.preventDefault()
-  hiddenSection.style.display = 'none'
-  form.reset()
-  let resultView = document.querySelector('#result-view')
-  resultView.innerHTML = ''
-  closeButton.style.display = 'none'
-}
-
-const handleFormDetails = (e) => {
-  hiddenSection.style.display = 'none'
   let firstName = document.querySelector('#firstName').value
   let lastName = document.querySelector('#lastName').value
   let email = document.querySelector('#email').value
   let age = document.querySelector('#age').value
-  let resultView = document.querySelector('#result-view')
 
   const userData = {
     FirstName: firstName,
@@ -39,11 +22,62 @@ const handleFormDetails = (e) => {
     Age: age,
   }
 
+  if (
+    userData.FirstName &&
+    userData.LastName &&
+    userData.Email &&
+    userData.Age
+  ) {
+    hiddenSection.style.display = 'flex'
+    hiddenSection.style.flexDirection = 'column'
+    hiddenSection.style.alignItems = 'center'
+    viewDetails.style.display = 'flex'
+    closeButton.style.display = 'flex'
+    successMessage.style.display = 'flex'
+    successMessage.style.color = 'green'
+  } else {
+    successMessage.innerHTML = `<p>Please fill in all fields</p>`
+    successMessage.style.color = 'red'
+    successMessage.style.display = 'flex'
+    successMessage.style.alignItems = 'center'
+    successMessage.style.justifyContent = 'center'
+    hiddenSection.style.display = 'flex'
+    viewDetails.style.display = 'none'
+    closeButton.style.display = 'flex'
+    closeButton.style.alignItems = 'center'
+  }
+}
+
+const closeHiddenSection = (e) => {
+  e.preventDefault()
+  hiddenSection.style.display = 'none'
+  form.reset()
+  resultView.innerHTML = ''
+  closeButton.style.display = 'none'
+}
+
+const handleFormDetails = (e) => {
+  e.preventDefault()
+  let firstName = document.querySelector('#firstName').value
+  let lastName = document.querySelector('#lastName').value
+  let email = document.querySelector('#email').value
+  let age = document.querySelector('#age').value
+
+  const userData = {
+    FirstName: firstName,
+    LastName: lastName,
+    Email: email,
+    Age: age,
+  }
+  resultView.innerHTML = ''
   for (const property in userData) {
-    console.log(`${property}: ${userData[property]}`)
     resultView.innerHTML += `<li><p>${property}: ${userData[property]}</p></li>`
   }
   closeButton.style.display = 'flex'
   closeButton.style.alignItems = 'center'
+  viewDetails.style.display = 'none'
+  successMessage.style.display = 'none'
 }
 submitButton.addEventListener('click', showHiddenSection)
+viewDetails.addEventListener('click', handleFormDetails)
+closeButton.addEventListener('click', closeHiddenSection)
